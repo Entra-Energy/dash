43ea6b9ddeb14dbd1982499598a62d4f7921b11c
Georgi <gk.manev@gmail.com>
2022-06-21 00:06:36 +0300

    up

--- project/dash_back/custom_filters.py
+++ project/dash_back/custom_filters.py
@@ -1,56 +1,22 @@
-import django_filters
-from dash_back import models as django_models
-from django_filters import rest_framework as filters
-from rest_framework import viewsets
+from django_filters import FilterSet, AllValuesFilter,IsoDateTimeFilter,NumberFilter,DateRangeFilter,DateFilter,CharFilter,DateTimeFilter
 
-class PriceFilter(filters.FilterSet):
+from dash_back.models import Post, Price
+
+
+class PostFilter(FilterSet):
+
+    start_date = IsoDateTimeFilter(field_name='created_date',lookup_expr=('gte'),)
+    end_date = IsoDateTimeFilter(field_name='created_date',lookup_expr=('lte'))
+    date_range = DateRangeFilter(field_name='created_date')
+    dev = AllValuesFilter(field_name='devId')
     class Meta:
-        model = django_models.Price
-        fields = {
-            'timestamp': ('lte', 'gte')
-        }
+        model = Post
+        fields = ('start_date','end_date','date_range','dev')
 
-    filter_overrides = {
-        django_models.Price: {
-            'filter_class': django_filters.IsoDateTimeFilter
-        },
-    }
-
-class PostFilter(filters.FilterSet):
+class PriceFilter(FilterSet):
+    start_date = IsoDateTimeFilter(field_name='timestamp',lookup_expr=('gte'),)
+    end_date = IsoDateTimeFilter(field_name='timestamp',lookup_expr=('lte'))
+    date_range = DateRangeFilter(field_name='timestamp')
     class Meta:
-        model = django_models.Post
-        fields = {
-            'created_date': ('lte', 'gte')
-        }
-
-
-
-
-
-
-
-
-
-
-# from django_filters import FilterSet, AllValuesFilter,IsoDateTimeFilter,NumberFilter,DateRangeFilter,DateFilter,CharFilter,DateTimeFilter
-#
-# from dash_back.models import Post, Price
-
-
-# class PostFilter(FilterSet):
-#
-#     start_date = IsoDateTimeFilter(field_name='created_date',lookup_expr=('gte'),)
-#     end_date = IsoDateTimeFilter(field_name='created_date',lookup_expr=('lte'))
-#     date_range = DateRangeFilter(field_name='created_date')
-#     dev = AllValuesFilter(field_name='devId')
-#     class Meta:
-#         model = Post
-#         fields = ('start_date','end_date','date_range','dev')
-#
-# class PriceFilter(FilterSet):
-#     start_date = IsoDateTimeFilter(field_name='timestamp',lookup_expr=('gte'),)
-#     end_date = IsoDateTimeFilter(field_name='timestamp',lookup_expr=('lte'))
-#     date_range = DateRangeFilter(field_name='timestamp')
-#     class Meta:
-#         model = Price
-#         fields = ['timestamp']
+        model = Price
+        fields = ['timestamp']
