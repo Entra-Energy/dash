4169d8431ec0ca4fd762c7f244119866d99d2bd1
Georgi <gk.manev@gmail.com>
2022-06-24 18:01:25 +0300

    up

---
+++ project/dash_back/tasks.py
@@ -0,0 +1,65 @@
+from celery.task.schedules import crontab # type: ignore
+from celery.decorators import periodic_task # type: ignore
+from celery.utils.log import get_task_logger # type: ignore
+from celery import shared_task #type: ignore
+
+from dash_back.utils import test
+#from photos.utils import emptying_variants_table
+
+logger = get_task_logger(__name__)
+
+
+# @shared_task()
+# def task_save_latest_flickr_image():
+#    """
+#    Saves latest image from Flickr
+#    """
+#    save_latest_flickr_image()
+#    logger.info("Saved image from Flickr")
+
+
+# @periodic_task(run_every=crontab(minute="*/30"))
+# def task_save_latest_flickr_image():
+#    """
+#    Saves latest image from Flickr
+#    """
+#    save_latest_flickr_image()
+#    logger.info("Saved image from Flickr")
+
+@periodic_task(
+    run_every=(crontab(minute='*/1')),
+    name="task_test",
+    ignore_result=True
+)
+def task_test():
+    """
+    test!!!
+    """
+    test()
+    logger.info("test!!!")
+
+
+# @periodic_task(
+#     run_every=(crontab(minute='*/2')),
+#     name="task_test",
+#     ignore_result=True
+# )
+# def task_test():
+#     print("it works!")
+#     logger.info("Woeks!!!")
+
+
+
+
+# @periodic_task(
+#     run_every=(crontab(minute='*/1440')),
+#     name="task_empty_variants_table",
+#     ignore_result=True
+# )
+
+# def task_empty_variants_table():
+#     """
+#     Emptying variants table every xxx min
+#     """
+#     emptying_variants_table()
+#     logger.info("emtying variants")
