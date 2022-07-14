d6038ac56033747cb98f7484b2c6ee3b8bec0599
Georgi <gk.manev@gmail.com>
2022-07-14 14:46:55 +0300

    up

--- frontend_vue/src/components/LineCharts.vue
+++ frontend_vue/src/components/LineCharts.vue
@@ -329,42 +329,7 @@ export default {
                 let url2 = ""
                 this.get_data_helper(this.nextP,url2)
               }
-              else {
-                console.log()
-                if(test == 'today'){
-                  this.option.xAxis.axisLabel.formatter = timeLineSet//'{HH}:{mm}'
-                  this.option.tooltip.formatter =  toolTipSet
-                  this.option.xAxis.splitNumber = 24
-                  let endStr = this.currDate.split("T")[0]+"T23:00:00.000Z"
-                  let endArr = [endStr,null]
-                  this.option.series[1].data.push(endArr)
-                  //console.log(endArr)
-                }
-                else if(test == 'month')
-                {
-                    this.option.xAxis.axisLabel.formatter =  '{dd}/{MMM}'
-                    let monthLenthArr = this.currDate.split("T")[0].split("-")
-                    monthLenthArr[2] = this.monthLenthDays.toString()
-                    let monthEnd = [monthLenthArr.join("-"),null]
 
-
-                    let monthBegin = [this.currYear+"-"+this.currMonth+"-"+'01',null]
-                    this.option.series[1].data[0]=monthBegin
-                    this.option.series[1].data.push(monthEnd)
-                    this.option.xAxis.splitNumber = 30
-                    //console.log(this.option.series[1].data)
-                }
-                else {
-                    let yearBegin = [this.currYear+"-"+"01"+"-"+"01"]
-                    let yearEnd = [this.currYear+"-"+"12"+"-"+"31"]
-                    this.option.series[1].data[0]=yearBegin
-                    this.option.series[1].data.push(yearEnd)
-                    this.option.xAxis.axisLabel.formatter =  '{MMM}'
-                    this.option.xAxis.splitNumber = 12
-
-                }
-
-              }
             })
 
       // }
@@ -400,6 +365,45 @@ export default {
       },
 
      getData() {
+       let test = this.param
+       if(test == 'today'){
+         this.option.xAxis.axisLabel.formatter = timeLineSet//'{HH}:{mm}'
+         this.option.tooltip.formatter =  toolTipSet
+         this.option.xAxis.splitNumber = 24
+         let endStr = this.currDate.split("T")[0]+"T23:00:00.000Z"
+         let endArr = [endStr,null]
+         this.option.series[1].data.push(endArr)
+         //console.log(endArr)
+       }
+       else if(test == 'month')
+       {
+           this.option.xAxis.axisLabel.formatter =  '{dd}/{MMM}'
+           let monthLenthArr = this.currDate.split("T")[0].split("-")
+           monthLenthArr[2] = this.monthLenthDays.toString()
+           let monthEnd = [monthLenthArr.join("-"),null]
+
+
+           let monthBegin = [this.currYear+"-"+this.currMonth+"-"+'01',null]
+           this.option.series[1].data[0]=monthBegin
+           this.option.series[1].data.push(monthEnd)
+           this.option.xAxis.splitNumber = 30
+           //console.log(this.option.series[1].data)
+       }
+       else {
+           let yearBegin = [this.currYear+"-"+"01"+"-"+"01"]
+           let yearEnd = [this.currYear+"-"+"12"+"-"+"31"]
+           this.option.series[1].data[0]=yearBegin
+           this.option.series[1].data.push(yearEnd)
+           this.option.xAxis.axisLabel.formatter =  '{MMM}'
+           this.option.xAxis.splitNumber = 12
+
+       }
+
+
+
+
+
+
        let path = this.$route.path
        if (path == '/dashboard')
        {
