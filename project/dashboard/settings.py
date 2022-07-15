71024e9fc392f4d1a06cfceb8ced25aecec07e5d
Georgi <gk.manev@gmail.com>
2022-07-15 15:48:30 +0300

    up

--- project/dashboard/settings.py
+++ project/dashboard/settings.py
@@ -77,8 +77,8 @@ TEMPLATES = [
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 400,
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 400,
 }
 
 WSGI_APPLICATION = 'dashboard.wsgi.application'
