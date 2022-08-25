8f275dec247538a3cbcc605fe4d4822d9ccecb06
Georgi <gk.manev@gmail.com>
2022-08-26 00:23:10 +0300

    up

--- project/dashboard/settings.py
+++ project/dashboard/settings.py
@@ -111,7 +111,7 @@ CELERY_RESULT_BACKEND = os.environ.get("CELERY_BACKEND", "redis://redis:6379/0")
 CELERY_BEAT_SCHEDULE = {
       'task_test_it': {
         'task': 'dash_back.tasks.task_test',
-        'schedule': crontab(hour=0, minute=10),
+        'schedule': crontab(hour=4, minute=10),
     },
     'task_schedule_it': {
       'task': 'dash_back.tasks.task_schedule',
