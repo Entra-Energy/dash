b5869d9438d7634acba03663c4001efdc5b55fd9
Georgi <gk.manev@gmail.com>
2023-02-06 22:13:56 +0200

    up

--- project/dash_back/management/commands/mymqtt.py
+++ project/dash_back/management/commands/mymqtt.py
@@ -41,6 +41,7 @@ class Command(BaseCommand):
         def on_message(client, userdata, msg):
             #print(msg.topic+" "+str(msg.payload))
             topic = msg.topic
+            print(topic)
             myList = topic.split('/')
             if myList[0] == 'data':
                 dev_id = myList[1]
