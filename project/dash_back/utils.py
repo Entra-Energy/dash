d64940ac295cc3af027a1b041c36133e67d7b4e3
Georgi <gk.manev@gmail.com>
2023-04-03 12:18:57 +0300

    up

--- project/dash_back/utils.py
+++ project/dash_back/utils.py
@@ -15,13 +15,14 @@ import paho.mqtt.publish as publish
 def update_db_coeff():
     date = "2023-03-20T17:00:00Z"
     sm_coeff = [{"sm-0001":120},{"sm-0002":320},{"sm-0003":400},{"sm-0004":200},{"sm-0006":200},{"sm-0008":200},{"sm-0009":80},{"sm-0010":60},{"sm-0011":60},{"sm-0015":60},{"sm-0016":250}]
-    query = Post.objects.filter(created_date__lte = date)
+    query = Post.objects.filter(created_date__lte = date, devId = 'sm-0001')
+    
     for obj in query:
-        print(obj)
-        if obj.devId == 'sm-0001':
-            u = Post.objects.get(id=obj.id)
-            u.value = obj.value*120
-            u.save()         
+        print(obj.id)     
+        u = Post.objects.get(id=obj.id)
+        print(u)
+        #u.value = obj.value*120
+        #u.save()         
     
 
 def get_curr_time():
