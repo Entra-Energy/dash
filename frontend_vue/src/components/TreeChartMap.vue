aba6d6e616b50115a5f44b4737526b36efe5288e
Georgi <gk.manev@gmail.com>
2023-05-02 13:01:56 +0300

    up

--- frontend_vue/src/components/TreeChartMap.vue
+++ frontend_vue/src/components/TreeChartMap.vue
@@ -38,6 +38,7 @@ props: {
 },
 
 param:'today',
+all:[],
 
 
 name: "TreeChartMap",
@@ -70,7 +71,18 @@ series: [
       type: 'tree',
       id: 0,
       name: 'tree1',
-      data: [],
+      data: [{
+            name: 'EnergoPro',
+            children: [
+                          {
+                          name: 'GN1',value:0,
+                          children: []
+                          },
+                          {name: 'GN2',
+                          children: []           
+                          },
+                      ]
+            }],
       top: '1%',
       left: '8%',
       bottom: '1%',
@@ -85,9 +97,11 @@ series: [
       label: {
         backgroundColor: "none",
         position: 'left',
-        verticalAlign: 'middle',
+        verticalAlign: 'bottom',
         align: 'right',
-        color: '#fff'
+        color: '#fff',
+        fontSize:13
+
       },
       leaves: {
         label: {
@@ -100,9 +114,9 @@ series: [
       emphasis: {
         focus: 'descendant'
       },
-      expandAndCollapse: true,
-      animationDuration: 550,
-      animationDurationUpdate: 750
+      expandAndCollapse: false,
+      //animationDuration: 550,
+      //animationDurationUpdate: 750
   },
 ],
 
@@ -114,137 +128,270 @@ series: [
 
 methods: { 
 
- },
+        pollData(){
+          this.polling = setInterval(() => {
+            //this.option.series[0].data[0].children[0].children = []
+            this.getData();
+        }, 10000)
+      },
+
+       getData() {
+      try {
+        axios
+        .get(
+          "http://64.225.100.195:8000/api/online/"
+        )
+        .then(response => response.data.online.forEach(el=>{
+            //this.posts.push(el)
+            //console.log(el.dev)
+            let found = this.all.find(element => element.id === el.dev)
+
+
+            if (found)
+            {
+              found.ready = el.ready
+              found.pow = el.pow
+              found.providing = el.providing
+              found.online = 'online'
+
+              if (found.ready == 1)
+              {
+                if (found.providing == 0)
+                {
+                found.online = 'ready'
+                }
+                else if (found.providing == 1)
+                {
+                  found.online = 'providing'
+                }
+              }
+              else if (found.ready == 0)
+              {
+                found.online = 'not-ready'
+              }
+              else{
+                found.online = 'offline'
+              }
+
+
+
+            }
+
+            //console.log(this.all[0].id)
+
+        }) )
+
+        
+
+      } catch (error) {
+        //console.log(error);
+      }
+      this.treeMap();
+
+      
+
+      
+    },
+
+    treeMap(){
+      console.log(this.all)
+      this.option.series[0].data[0].children[0].children = []
+      this.option.series[0].data[0].children[1].children = []
+      let GN1Count = 0
+      let GN2Count = 0
+       this.all.forEach(el=>{
+        if(el.online != "offline"){
+        switch(el.id){
+            case "sm-0001":      
+     
+            GN1Count += parseFloat(el.pow)
+            this.option.series[0].data[0].children[0].children.push({'name' :el.id +" " + el.pow,'value':el.pow})
+            break
+            case "sm-0002":
+            GN2Count += parseFloat(el.pow)
+            this.option.series[0].data[0].children[1].children.push({'name' :el.id +" " + el.pow,'value':el.pow})
+            break
+            case "sm-0003":
+            GN1Count += parseFloat(el.pow)            
+            this.option.series[0].data[0].children[0].children.push({'name' :el.id +" " + el.pow,'value':el.pow})
+            break
+          case "sm-0004":
+            GN1Count += parseFloat(el.pow) 
+            this.option.series[0].data[0].children[0].children.push({'name' :el.id +" " + el.pow,'value':el.pow})
+            break
+          case "sm-0006":
+            GN2Count += parseFloat(el.pow) 
+            this.option.series[0].data[0].children[1].children.push({'name' :el.id +" " + el.pow,'value':el.pow})
+            break
+          case "sm-0007":
+            GN1Count += parseFloat(el.pow) 
+            this.option.series[0].data[0].children[0].children.push({'name' :el.id +" " + el.pow,'value':el.pow})
+            break
+          case "sm-0008":
+            GN1Count += parseFloat(el.pow) 
+            this.option.series[0].data[0].children[0].children.push({'name' :el.id +" " + el.pow,'value':el.pow})
+            break
+          case "sm-0009":
+            GN1Count += parseFloat(el.pow)  
+            this.option.series[0].data[0].children[0].children.push({'name' :el.id +" " + el.pow,'value':el.pow})
+            break
+          case "sm-0010":
+            GN2Count += parseFloat(el.pow) 
+            this.option.series[0].data[0].children[1].children.push({'name' :el.id +" " + el.pow,'value':el.pow})
+            break
+          case "sm-0011":
+            GN2Count += parseFloat(el.pow) 
+            this.option.series[0].data[0].children[1].children.push({'name' :el.id +" " + el.pow,'value':el.pow})
+            break
+          case "sm-0012":
+            GN1Count += parseFloat(el.pow) 
+            this.option.series[0].data[0].children[0].children.push({'name' :el.id +" " + el.pow,'value':el.pow})
+            break
+          case "sm-0013":
+            GN2Count += parseFloat(el.pow) 
+            this.option.series[0].data[0].children[1].children.push({'name' :el.id +" " + el.pow,'value':el.pow})
+            break
+          case "sm-0015":
+            GN2Count += parseFloat(el.pow) 
+            GN2Count += parseFloat(el.pow) 
+            this.option.series[0].data[0].children[1].children.push({'name' :el.id +" " + el.pow,'value':el.pow})
+            break
+          case "sm-0016":
+            GN2Count += parseFloat(el.pow) 
+            this.option.series[0].data[0].children[1].children.push({'name' :el.id +" " + el.pow,'value':el.pow})
+            break
+          case "sm-0017":
+            GN1Count += parseFloat(el.pow) 
+            this.option.series[0].data[0].children[0].children.push({'name' :el.id +" " + el.pow,'value':el.pow})
+            break
+          case "sm-0018":
+            GN1Count += parseFloat(el.pow) 
+            this.option.series[0].data[0].children[0].children.push({'name' :el.id +" " + el.pow,'value':el.pow})
+            break
+          case "sm-0019":
+            GN2Count += parseFloat(el.pow) 
+            this.option.series[0].data[0].children[1].children.push({'name' :el.id +" " + el.pow,'value':el.pow})
+            break
+          case "sm-0020":
+            GN2Count += parseFloat(el.pow) 
+            this.option.series[0].data[0].children[1].children.push({'name' :el.id +" " + el.pow,'value':el.pow})
+            break
+          case "sm-0024":
+            GN1Count += parseFloat(el.pow) 
+            this.option.series[0].data[0].children[0].children.push({'name' :el.id +" " + el.pow,'value':el.pow})
+            break
+          case "sm-0025":
+            GN1Count += parseFloat(el.pow) 
+            this.option.series[0].data[0].children[0].children.push({'name' :el.id +" " + el.pow,'value':el.pow})
+            break
+        }
+        this.option.series[0].data[0].children[0].value = GN1Count
+        this.option.series[0].data[0].children[1].value = GN2Count
+      }
+
+      })
+    }
+  
+
+},
 
 
 
 created (){
 
-const data = {
-  name: 'flare',
-  children: [
-    {
-      name: 'data',
-      children: [
-        {
-          name: 'converters',
-          children: [
-            { name: 'Converters', value: 721 },
-            { name: 'DelimitedTextConverter', value: 4294 }
-          ]
-        },
-        {
-          name: 'DataUtil',
-          value: 3322
-        }
-      ]
-    },
-    {
-      name: 'display',
-      children: [
-        { name: 'DirtySprite', value: 8833 },
-        { name: 'LineSprite', value: 1732 },
-        { name: 'RectSprite', value: 3623 }
-      ]
-    },
-    {
-      name: 'flex',
-      children: [{ name: 'FlareVis', value: 4116 }]
-    },
-    {
-      name: 'query',
-      children: [
-        { name: 'AggregateExpression', value: 1616 },
-        { name: 'And', value: 1027 },
-        { name: 'Arithmetic', value: 3891 },
-        { name: 'Average', value: 891 },
-        { name: 'BinaryExpression', value: 2893 },
-        { name: 'Comparison', value: 5103 },
-        { name: 'CompositeExpression', value: 3677 },
-        { name: 'Count', value: 781 },
-        { name: 'DateUtil', value: 4141 },
-        { name: 'Distinct', value: 933 },
-        { name: 'Expression', value: 5130 },
-        { name: 'ExpressionIterator', value: 3617 },
-        { name: 'Fn', value: 3240 },
-        { name: 'If', value: 2732 },
-        { name: 'IsA', value: 2039 },
-        { name: 'Literal', value: 1214 },
-        { name: 'Match', value: 3748 },
-        { name: 'Maximum', value: 843 },
-        {
-          name: 'methods',
-          children: [
-            { name: 'add', value: 593 },
-            { name: 'and', value: 330 },
-            { name: 'average', value: 287 },
-            { name: 'count', value: 277 },
-            { name: 'distinct', value: 292 },
-            { name: 'div', value: 595 },
-            { name: 'eq', value: 594 },
-            { name: 'fn', value: 460 },
-            { name: 'gt', value: 603 },
-            { name: 'gte', value: 625 },
-            { name: 'iff', value: 748 },
-            { name: 'isa', value: 461 },
-            { name: 'lt', value: 597 },
-            { name: 'lte', value: 619 },
-            { name: 'max', value: 283 },
-            { name: 'min', value: 283 },
-            { name: 'mod', value: 591 },
-            { name: 'mul', value: 603 },
-            { name: 'neq', value: 599 },
-            { name: 'not', value: 386 },
-            { name: 'or', value: 323 },
-            { name: 'orderby', value: 307 },
-            { name: 'range', value: 772 },
-            { name: 'select', value: 296 },
-            { name: 'stddev', value: 363 },
-            { name: 'sub', value: 600 },
-            { name: 'sum', value: 280 },
-            { name: 'update', value: 307 },
-            { name: 'variance', value: 335 },
-            { name: 'where', value: 299 },
-            { name: 'xor', value: 354 },
-            { name: 'x_x', value: 264 }
-          ]
-        },
-        { name: 'Minimum', value: 843 },
-        { name: 'Not', value: 1554 },
-        { name: 'Or', value: 970 },
-        { name: 'Query', value: 13896 },
-        { name: 'Range', value: 1594 },
-        { name: 'StringUtil', value: 4130 },
-        { name: 'Sum', value: 791 },
-        { name: 'Variable', value: 1124 },
-        { name: 'Variance', value: 1876 },
-        { name: 'Xor', value: 1101 }
-      ]
-    },
-    {
-      name: 'scale',
-      children: [
-        { name: 'IScaleMap', value: 2105 },
-        { name: 'LinearScale', value: 1316 },
-        { name: 'LogScale', value: 3151 },
-        { name: 'OrdinalScale', value: 3770 },
-        { name: 'QuantileScale', value: 2435 },
-        { name: 'QuantitativeScale', value: 4839 },
-        { name: 'RootScale', value: 1756 },
-        { name: 'Scale', value: 4268 },
-        { name: 'ScaleType', value: 1821 },
-        { name: 'TimeScale', value: 5833 }
-      ]
-    }
-  ]
-};
-this.option.series[0].data = [data]
-console.log(this.option.series)
+  this.all = this.$store.state.allDevs
+  this.pollData();
 
 },
 computed: {
-
+  
 },
+beforeDestroy () {
+	   clearInterval(this.polling)
+  },
+
+// watch: {
+//    '$store.state.allDevs': {
+//       immediate: true,
+//       handler() {           
+//        let all = this.$store.state.allDevs
+      
+//        let GN1Count = 0
+//        all.forEach(el=>{
+
+//         switch(el.id){
+//           case "sm-0001":
+//             GN1Count += parseFloat(el.pow)
+//             this.option.series[0].data[0].children[0].children.push({'name':el.id,'value':el.pow})
+//             break
+//           case "sm-0002":
+//             this.option.series[0].data[0].children[1].children.push({'name':el.id,'value':el.pow})
+//             break
+//           case "sm-0003":
+//             GN1Count += parseFloat(el.pow)
+//             this.option.series[0].data[0].children[0].children.push({'name':el.id,'value':el.pow})
+//             break
+//           case "sm-0004":
+//             this.option.series[0].data[0].children[0].children.push({'name':el.id,'value':el.pow})
+//             break
+//           case "sm-0006":
+//             this.option.series[0].data[0].children[1].children.push({'name':el.id,'value':el.pow})
+//             break
+//           case "sm-0007":
+//             this.option.series[0].data[0].children[0].children.push({'name':el.id,'value':el.pow})
+//             break
+//           case "sm-0008":
+//             this.option.series[0].data[0].children[0].children.push({'name':el.id,'value':el.pow})
+//             break
+//           case "sm-0009":
+//             this.option.series[0].data[0].children[0].children.push({'name':el.id,'value':el.pow})
+//             break
+//           case "sm-0010":
+//             this.option.series[0].data[0].children[1].children.push({'name':el.id,'value':el.pow})
+//             break
+//           case "sm-0011":
+//             this.option.series[0].data[0].children[1].children.push({'name':el.id,'value':el.pow})
+//             break
+//           case "sm-0012":
+//             this.option.series[0].data[0].children[0].children.push({'name':el.id,'value':el.pow})
+//             break
+//           case "sm-0013":
+//             this.option.series[0].data[0].children[1].children.push({'name':el.id,'value':el.pow})
+//             break
+//           case "sm-0015":
+//             this.option.series[0].data[0].children[1].children.push({'name':el.id,'value':el.pow})
+//             break
+//           case "sm-0016":
+//             this.option.series[0].data[0].children[1].children.push({'name':el.id,'value':el.pow})
+//             break
+//           case "sm-0017":
+//             this.option.series[0].data[0].children[0].children.push({'name':el.id,'value':el.pow})
+//             break
+//           case "sm-0018":
+//             this.option.series[0].data[0].children[0].children.push({'name':el.id,'value':el.pow})
+//             break
+//           case "sm-0019":
+//             this.option.series[0].data[0].children[1].children.push({'name':el.id,'value':el.pow})
+//             break
+//           case "sm-0020":
+//             this.option.series[0].data[0].children[1].children.push({'name':el.id,'value':el.pow})
+//             break
+//           case "sm-0024":
+//             this.option.series[0].data[0].children[0].children.push({'name':el.id,'value':el.pow})
+//             break
+//           case "sm-0025":
+//             this.option.series[0].data[0].children[0].children.push({'name':el.id,'value':el.pow})
+//             break
+//         }
+//        })
+//        //this.option.series[0].data[0].children[0].children.push({'name':el.id,'value':el.pow})
+
+
+       
+        
+//       },
+//     }
+//    },
 
 
 
