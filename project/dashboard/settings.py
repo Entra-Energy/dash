a57fda8bc07b0cc9ba4411c6afdd69c67258baa3
Georgi <gk.manev@gmail.com>
2023-09-25 15:09:28 +0300

    up

--- project/dashboard/settings.py
+++ project/dashboard/settings.py
@@ -139,7 +139,7 @@ CELERY_BEAT_SCHEDULE = {
         },
     'task_auto_forecast':{
         'task':'dash_back.tasks.task_auto_forecast',
-        'schedule': crontab(hour=15, minute=4),
+        'schedule': crontab(hour=15, minute=13),
     }
     # 'task_simavi_fetch_data':{
     #     'task': 'dash_back.tasks.task_simavi',
